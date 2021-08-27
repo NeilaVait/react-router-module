364c577ae8449dea234ca977fcc1e1d35457aaf0
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-27 15:23:22 +0300

    asc-desc button works

--- src/components/quotes/QuoteList.jsx
+++ src/components/quotes/QuoteList.jsx
@@ -1,12 +1,28 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
+  const [quotes, setQuotes] = useState(props.quotes);
+  const [direction, setDirection] = useState('asc');
+
+  const sortHandler = () => {
+    if (direction === 'desc') {
+      setDirection('asc');
+      return quotes.sort((a, b) => b.author.localeCompare(a.author));
+    } else if (direction === 'asc') {
+      setDirection('desc');
+      return quotes.sort((a, b) => a.author.localeCompare(b.author));
+    }
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={sortHandler}>sort {direction}</button>
+      </div>
       <ul className={classes.list}>
-        {(props.quotes || []).map((quote) => (
+        {(quotes || []).map((quote) => (
           <QuoteItem key={quote.id} id={quote.id} author={quote.author} text={quote.text} />
         ))}
       </ul>
