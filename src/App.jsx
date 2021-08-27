45d17d39d02205cad53a113e9ed302b19fd47f92
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-27 11:27:20 +0300

    layout done

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import AllQuotes from './components/pages/AllQuotes';
 import QuoteDetails from './components/pages/QuoteDetails';
 import NewQuote from './components/pages/NewQuote';
+import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
+    <Layout>
       <Switch>
         <Route path="/" exact>
           <Redirect to="/quotes" />
@@ -20,7 +22,7 @@ function App() {
           <NewQuote />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainNavigation from './MainNavigation';
+import cl from './Layout.module.css';
+
+const Layout = (props) => {
+  return (
+    <>
+      <MainNavigation />
+      <main className={cl.main}>{props.children}</main>
+    </>
+  );
+};
+
+export default Layout;
